d8903ccb76319e789e6e88510c36713acf1f8d6e
juby91@live.com <juby91@live.com>
2016-02-15 19:52:14 -0800

    Initial Commit

---
+++ QuikFix/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QuikFix
+//
+//  Created by Susan Salas on 2/15/16.
+//  Copyright © 2016 Susan Salas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
