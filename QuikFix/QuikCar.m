32c647c8b9a69ee5e78ee32eed2f95ec7f3e7b87
Sean Barry <sfbarry1@gmail.com>
2016-02-17 15:40:29 -0800

    add car to database

--- QuikFix/QuikCar.m
+++ QuikFix/QuikCar.m
@@ -7,7 +7,23 @@
 //
 
 #import "QuikCar.h"
+#import "Firebase/Firebase.h"
 
 @implementation QuikCar
+
+-(void)addCarToDatabase{
+    NSDictionary *carDict = @{@"vin":self.vin,
+                                     @"year":self.year,
+                                     @"make":self.make,
+                                     @"model":self.model,
+                                     @"body":self.body,
+                                     @"color":@"red",
+                                     @"license":self.license};
+
+    NSDictionary *topLevel = @{self.vin:carDict};
+
+    Firebase *ref = [[Firebase alloc] initWithUrl:@"https://beefstagram.firebaseio.com/cars"];
+    [ref setValue:topLevel];
+}
 
 @end
