5da233b1f035ef2e77d2fd88173c63ddf310ad88
Sean Barry <sfbarry1@gmail.com>
2016-02-20 20:12:19 -0800

    add cameraVC

--- QuikFix/AFTableViewCell.h
+++ QuikFix/AFTableViewCell.h
@@ -1,6 +1,5 @@
 //
 //  AFTableViewCell.h
-//  AFTabledCollectionView
 //
 //  Created by Ash Furrow on 2013-03-14.
 //  Copyright (c) 2013 Ash Furrow. All rights reserved.
---
+++ QuikFix/QuikUserImagePickerVC.m
@@ -0,0 +1,83 @@
+//
+//  QuikUserImagePickerVC.m
+//  QuikFix
+//
+//  Created by Sean Barry on 2/20/16.
+//  Copyright © 2016 Susan Salas. All rights reserved.
+//
+
+#import "QuikUserImagePickerVC.h"
+
+@interface QuikUserImagePickerVC ()
+
+@end
+
+@implementation QuikUserImagePickerVC
+
+- (void)viewDidLoad {
+
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+
+    if (![UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+
+        UIAlertView *myAlertView = [[UIAlertView alloc] initWithTitle:@"Error"
+                                                              message:@"Device has no camera"
+                                                             delegate:nil
+                                                    cancelButtonTitle:@"OK"
+                                                    otherButtonTitles: nil];
+
+        [myAlertView show];
+
+    }
+
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+
+    [super viewDidAppear:animated];
+
+}
+
+- (IBAction)onCameraTapped:(UIButton *)sender {
+
+    UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+    picker.delegate = self;
+    picker.allowsEditing = YES;
+    picker.sourceType = UIImagePickerControllerSourceTypeCamera;
+
+    [self presentViewController:picker animated:YES completion:NULL];
+
+}
+
+- (IBAction)onLibraryTapped:(UIButton *)sender {
+
+    UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+    picker.delegate = self;
+    picker.allowsEditing = YES;
+    picker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+
+    [self presentViewController:picker animated:YES completion:NULL];
+
+
+}
+
+#pragma mark - Image Picker Controller delegate methods
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
+
+    UIImage *chosenImage = info[UIImagePickerControllerEditedImage];
+    self.imageView.image = chosenImage;
+
+    [picker dismissViewControllerAnimated:YES completion:NULL];
+
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+
+    [picker dismissViewControllerAnimated:YES completion:NULL];
+    
+}
+
+
+@end
