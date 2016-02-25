2c39e05de7f84417d8c3f1fe204c0f62cc5c9b2a
EvgenyShkuratov <evgeny.shkuratov@gmail.com>
2016-02-25 15:39:58 -0800

    passing images to QuikClaimDescriptionVC

---
+++ QuikFix/QuikClaimDescriptionBigPicVC.h
@@ -0,0 +1,15 @@
+//
+//  QuikClaimDescriptionBigPicVC.h
+//  QuikFix
+//
+//  Created by Sujin Oh on 2/24/16.
+//  Copyright © 2016 Susan Salas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "QuikClaim.h"
+
+@interface QuikClaimDescriptionBigPicVC : UIViewController
+@property UIImage *currentImage;
+
+@end
